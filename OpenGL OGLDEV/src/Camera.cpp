397e5b4204bfc20131124111f5f55327a3162080
orangefrog45 <sambaker860@gmail.com>
2023-02-05 20:50:46 +0000

    Fixed y-axis lookAt flip bug

--- OpenGL OGLDEV/src/Camera.cpp
+++ OpenGL OGLDEV/src/Camera.cpp
@@ -8,18 +8,6 @@
 #include "KeyboardState.h"
 #include "TimeStep.h"
 
-/*Camera::Camera(const glm::fvec3& Pos, const glm::fvec3& Target, const glm::fvec3& Up) {
-
-	m_windowWidth = WindowWidth;
-	m_windowHeight = WindowHeight;
-
-	m_pos = Pos;
-	m_target = Target;
-	glm::normalize(m_target);
-	m_up = Up;
-	glm::normalize(m_up);
-
-}*/
 
 Camera::Camera(int windowWidth, int windowHeight, TimeStep* timeStep) : m_windowWidth(windowWidth), m_windowHeight(windowHeight), timeStep(timeStep), m_speed(0.001f) {
 	m_target = glm::fvec3(0.0f, 0.0f, -1.0f);
@@ -36,7 +24,6 @@ void Camera::SetPosition(float x, float y, float z) {
 void Camera::HandleInput(KeyboardState* keyboard) {
 
 	KeyboardState keyboardState = *keyboard;
-	if (!keyboardState.shiftPressed && !keyboardState.ctrlPressed) {
 	if (keyboardState.wPressed == true) {
 		MoveForward();
 	};
@@ -49,7 +36,6 @@ void Camera::HandleInput(KeyboardState* keyboard) {
 	if (keyboardState.dPressed == true) {
 		StrafeRight();
 	};
-	}
 	if (keyboardState.ePressed == true) {
 		MoveUp();
 	};
@@ -60,12 +46,18 @@ void Camera::HandleInput(KeyboardState* keyboard) {
 
 void Camera::OnMouse(const glm::vec2& newMousePos) {
 	const float rotationSpeed = 0.005f;
+	float maxDelta = 10.0f;
+
 	glm::vec2 mouseDelta = glm::vec2(newMousePos.x-m_windowWidth/2, newMousePos.y-m_windowHeight/2);
-	if (mouseDelta.x > -10.0f && mouseDelta.x < 10.0f ) {
-	m_target = glm::rotate(mouseDelta.x * rotationSpeed, m_up) * glm::fvec4(m_target, 0);
+	if (mouseDelta.x > -maxDelta && mouseDelta.x < maxDelta ) {
+		m_target = glm::rotate(mouseDelta.x * rotationSpeed, m_up) * glm::fvec4(m_target, 0);
 	}
-	if (mouseDelta.y > -10.0f && mouseDelta.y < 10.0f) {
-	m_target = glm::rotate(mouseDelta.y * rotationSpeed, glm::cross(m_target, m_up)) * glm::fvec4(m_target, 0);
+	if (mouseDelta.y > -maxDelta && mouseDelta.y < maxDelta) {
+		glm::fvec3 m_targetNew = glm::rotate(mouseDelta.y * rotationSpeed, glm::cross(m_target, m_up)) * glm::fvec4(m_target, 0);
+		//constraint to stop lookAt flipping from y axis alignment
+		if (m_targetNew.y <= 0.99f && m_targetNew.y >= -0.99f) {
+			m_target = m_targetNew;
+		}
 	}
 
 	glm::normalize(m_target);
@@ -95,17 +87,6 @@ void Camera::MoveDown() {
 }
 
 glm::fmat4x4 Camera::GetMatrix() {
-
-	glm::fmat4x4 cameraTransMatrix(
-		1.0f, 0.0f, 0.0f, -m_pos.x,
-		0.0f, 1.0f, 0.0f, -m_pos.y,
-		0.0f, 0.0f, 1.0f, -m_pos.z,
-		0.0f, 0.0f, 0.0f, 1.0f
-	);
-
-
-	//glm::fmat4x4 cameraTransform = ExtraMath::Init3DCameraTransform(m_pos, m_target, m_up);
-	//return cameraTransform;
 	return glm::rowMajor4(glm::lookAt(m_pos, m_pos + m_target, m_up));;
 }
 
